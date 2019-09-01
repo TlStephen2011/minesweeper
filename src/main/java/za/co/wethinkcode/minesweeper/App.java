2357bf1c5e7b282e2b05e5f45254f3fb64a5f0c8
tstephen <tstephen@iqbusiness.net>
2019-09-01 21:22:59 +0200

    added basic gui

--- src/main/java/za/co/wethinkcode/minesweeper/App.java
+++ src/main/java/za/co/wethinkcode/minesweeper/App.java
@@ -1,12 +1,26 @@
 package za.co.wethinkcode.minesweeper;
 
+import javax.swing.SwingUtilities;
+
 import za.co.wethinkcode.minesweeper.game.Board;
+import za.co.wethinkcode.minesweeper.gui.MainWindow;
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	Board b = new Board(10);
-    	b.printBoard();
+    	
+    	SwingUtilities.invokeLater(new StartGame());
+    	
+    	//Board b = new Board(10);
+    	//b.printBoard();
     }
+}
+
+class StartGame implements Runnable {
+
+	public void run() {
+		new MainWindow();
+	}
+	
 }
--- src/main/java/za/co/wethinkcode/minesweeper/gui/MainWindow.java
+++ src/main/java/za/co/wethinkcode/minesweeper/gui/MainWindow.java
@@ -1,7 +1,39 @@
 package za.co.wethinkcode.minesweeper.gui;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class MainWindow extends JFrame {
 
+	int gridSize = 100;
+	
+	ArrayList<JButton> btns = new ArrayList<JButton>(gridSize);
+	
+	public MainWindow() {
+		super("Minesweeper");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+		setResizable(false);
+		
+		for (int i = 0; i < gridSize; i++) {
+			JButton btn = new JButton();
+			// Change size of buttons
+			btn.setPreferredSize(new Dimension(30, 30));
+			btn.setMargin(new Insets(0, 0, 0, 0));
+			
+			getContentPane().add(btn);
+			btns.add(btn);						
+		}
+		
+		// Figure out calculation
+		setSize(new Dimension(306, 300 + 30));
+		setVisible(true);
+	}	
 }
+
