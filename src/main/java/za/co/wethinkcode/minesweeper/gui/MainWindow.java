d12f019a0cdc41ab84af5ca24bf7ed960b02c3ad
tstephen <tstephen@iqbusiness.net>
2019-09-01 21:48:30 +0200

    Added mouse event to buttons

--- src/main/java/za/co/wethinkcode/minesweeper/gui/MainWindow.java
+++ src/main/java/za/co/wethinkcode/minesweeper/gui/MainWindow.java
@@ -2,16 +2,20 @@ package za.co.wethinkcode.minesweeper.gui;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 public class MainWindow extends JFrame {
 
-	int cellsInRow = 30;
+	int cellsInRow = 10;
 	int buttonSize = 30;
 	int numCells = cellsInRow * cellsInRow;
 	
@@ -24,16 +28,32 @@ public class MainWindow extends JFrame {
 		setResizable(false);
 		
 		for (int i = 0; i < numCells; i++) {
-			JButton btn = new JButton();
+			final JButton btn = new JButton();
 			// Change size of buttons
 			btn.setPreferredSize(new Dimension(buttonSize, buttonSize));
 			btn.setMargin(new Insets(0, 0, 0, 0));
+			btn.setFocusPainted(false);
+			
+			//TODO: might remove
+			btn.setFont(new Font("Arial", Font.PLAIN, 18));
+			
+			btn.addMouseListener(new MouseAdapter(){
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					if (SwingUtilities.isRightMouseButton(e)) {
+						btn.setText("!");
+					} else {
+						btn.setText("-");
+					}
+				}
+				
+	        });
 			
 			getContentPane().add(btn);
 			btns.add(btn);						
 		}
 		
-		// Figure out calculation
 		getContentPane().setPreferredSize(new Dimension(cellsInRow * buttonSize, cellsInRow * buttonSize));
 		pack();
 		setVisible(true);		
